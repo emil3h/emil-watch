3562fea7d05ebd68c2cdd187891272d1351eba29
Email-12 <emil3h@gmail.com>
2026-02-22 02:40:10 -0800

    added 4 new watches

--- data.js
+++ data.js
@@ -11,6 +11,28 @@ export const watches = [
     },
     {
         id: 2,
+        brand: "Seiko",
+        model: "TicTac 35th Anniversary SZSB007",
+        image: "https://img.chrono24.com/images/uhren/35192376-0s7x55nuw5z2qszjakxq1d8e-ExtraLarge.jpg",
+        price: "$699",
+        movement: "Automatic 4R35",
+        link: "https://www.chrono24.com/seiko/seiko-seikotictac-commemorative-collaboration-szsb007-automatic-watch-for-men--id40497727.htm?searchHash=4178f52_zhymqv&pos=3",
+        description: "A JDM limited edition commemorating TicTac's 35th anniversary, featuring a stunning grey sunburst dial and elegant cathedral hands.",
+        imageStyle: "transform: scale(1.25);"
+    },
+    {
+        id: 3,
+        brand: "Unimatic",
+        model: "Modello Quattro UC4",
+        image: "https://www.unimaticwatches.com/wp-content/uploads/2023/04/UC-4-Hero.png",
+        price: "$655",
+        movement: "Automatic Seiko NH35A",
+        link: "https://www.unimaticwatches.com/uc4/",
+        description: "An Italian-made military watch with 300m water resistance.",
+        imageStyle: "transform: scale(1.5);"
+    },
+    {
+        id: 4,
         brand: "Tissot",
         model: "Tissot Gentleman 40",
         image: "https://www.tissotwatches.com/dw/image/v2/BKKD_PRD/on/demandware.static/-/Sites-Tissot-Catalogue/default/dw5fe3c7da/product-pictures/fbdb243e-3388-428d-b769-ed860477fd07_T127-407-11-051-00_shadow.png?sm=fit&sw=800&sh=800,gravity=center",
@@ -21,7 +43,7 @@ export const watches = [
         imageStyle: "transform: scale(1.2);"
     },
     {
-        id: 3,
+        id: 5,
         brand: "Seiko",
         model: "5 Sports Field SRPG27",
         image: "https://teddybaldassarre.com/cdn/shop/files/1_0041b998-de34-4e41-8f82-bf2579dba061_1000x.jpg?v=1738266660",
@@ -53,7 +75,7 @@ export const watches = [
         description: "A classic dress watch with vintage-inspired design."
     },
     {
-        id: 6,
+        id: 7,
         brand: "Rolex",
         model: "Submariner Date",
         image: "https://media.rolex.com/image/upload/q_auto:eco/f_auto/$path_!v1:catalogue:2025:upright-c:m126610ln-0001!/t_v7-config-with-shadow-landscape/c_limit,w_800/v1/catalogue/2025/upright-c-shadow/m126610ln-0001",
@@ -74,7 +96,7 @@ export const watches = [
         description: "The round watch with the many right angles."
     },
     {
-        id: 8,
+        id: 9,
         brand: "Omega",
         model: "Seamaster 300 41",
         image: "https://www.omegawatches.com/media/catalog/product/o/m/omega-seamaster-300-co-axial-master-chronometer-41-mm-23432412101001-e4e87d.png",
@@ -85,7 +107,7 @@ export const watches = [
         imageStyle: "transform: scale(1.15);"
     },
     {
-        id: 9,
+        id: 10,
         brand: "Omega",
         model: "Speedmaster Moonwatch Professional 42",
         image: "https://www.omegawatches.com/media/catalog/product/o/m/omega-speedmaster-moonwatch-professional-co-axial-master-chronometer-chronograph-42-mm-31032425001002-78e3aa.png",
@@ -96,7 +118,7 @@ export const watches = [
         imageStyle: "transform: scale(1.15);"
     },
     {
-        id: 10,
+        id: 11,
         brand: "Casio",
         model: "CA500WEBF-1A",
         image: "https://www.casio.com/content/dam/casio/product-info/locales/us/en/timepiece/product/watch/C/CA/CA5/ca-500webf-1a/assets/CA-500WEBF-1A.png.transform/main-visual-pc/image.png",
@@ -105,5 +127,27 @@ export const watches = [
         link: "https://www.casio.com/us/watches/casio/product.CA-500WEBF-1A/",
         description: "A retro-style calculator watch with a vintage design.",
         imageStyle: "transform: scale(1.1);"
+    },
+    {
+        id: 12,
+        brand: "Brew",
+        model: "Metric Retro Dial",
+        image: "https://www.brew-watches.com/cdn/shop/files/BREWMETRICFRONTRETRODIAL-SD_2048x2048.jpg?v=1700672505",
+        price: "$475",
+        movement: "Hybrid VK68 Meca-Quartz",
+        link: "https://www.brew-watches.com/watches/brew-metric-retro-black",
+        description: "A compact 36mm chronograph inspired by 1970s coffee culture, with a unique espresso extraction timer marking the sweet spot.",
+        imageStyle: "transform: scale(1.2);"
+    },
+    {
+        id: 13,
+        brand: "Seiko",
+        model: "5 Sports SRPE61",
+        image: "https://www.seikowatches.com/us-en/-/media/Images/Product--Image/All/Seiko/2022/02/20/02/24/SRPE61K1/SRPE61K1.png",
+        price: "$295",
+        movement: "Automatic 4R36",
+        link: "https://www.seikowatches.com/us-en/products/5sports/srpe61",
+        description: "A compact 5 Sports model with gray sunray dial and nylon strap.",
+        imageStyle: "transform: scale(1.0);"
     }
 ];
